7f640bdbff1bffa52bc194a83141438513fa3d07
Sean Langton <slangton1@gmail.com>
2018-10-27 02:49:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,9 @@
 def oxford_comma(array)
-if array.size = 2 then return "#{array[0]} and #{array[1]}"
-  array.join(", ")
+if array.size = 2 then return "#{array[0]} and #{array[1]}" 
+elsif array.size = 3 then return "#{array[0]}, #{array[1]} and #{array[2]}"
+else  
+      last_word = array.pop
+      new_array = array.join(", ")
+      new_array << "and #{last_word}"
+      return new_array
 end
