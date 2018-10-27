ab2d3d93c952d9f10cc2126280b6211446e22888
Sean Langton <slangton1@gmail.com>
2018-10-27 02:51:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+if    array.size == 1 then return "#{array[0]}"
+elsif array.size == 2 then return "#{array[0]} and #{array[1]}"
+elsif array.size == 3 then return "#{array[0]}, #{array[1]}, and #{array[2]}"
+else
+      last_word = array.pop
+      new_array = array.join(", ")
+      new_array << ", and #{last_word}"
+      return new_array
+    end
 end
